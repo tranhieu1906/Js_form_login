23da5b317a424ae610c905fbe23e571cb61dd135
Tran Hieu <nguyentranhieugttn@gmail.com>
2022-09-09 11:34:32 +0700

    #2

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 function Btn(){
+    if (submitName() === true && submitpass() === true&&confirmPass() === true && confirmtel() === true){
+        alert("Bạn đã đăng kí thành công")
+    }
     submitName()
     submitpass()
     confirmPass()
     confirmtel()
     confirmEmail()
-
+    submitLocation()
 }
 function submitName(){
     let name = document.getElementById("name").value
-    if (name == ""){
-        document.getElementById("ntName").innerHTML="chưa nhập tên kìa"
+    if (name != ""){
+        document.getElementById("ntName").innerHTML=""
+    }else if (name == ""){
+        document.getElementById("ntName").innerHTML="chưa nhập tên "
+    }else {
+        return true
     }
+
 }
 function submitpass(){
     let pass = document.getElementById("password").value
     if (pass == ""){
-        document.getElementById("ntPass").innerHTML="chưa nhập pass kìa"
+        document.getElementById("ntPass").innerHTML="chưa nhập password "
     }else if (pass.length < 6 || pass.length > 20){
         document.getElementById("ntPass").innerHTML="bạn cần nhập trên 6 ký tự và dưới 20 ký tự "
+    }else {
+        return true
     }
 }
 function confirmPass(){
@@ -25,35 +35,39 @@ function confirmPass(){
     let confirmPass = document.getElementById("confirmPassword").value;
     if (confirmPass != pass){
         document.getElementById("ntPassword").innerHTML="mật khẩu nhập lại không đúng"
+    }else {
+        return true
     }
 }
 function confirmtel(){
     let tel = document.getElementById("tel").value
-
-    if(tel.length === 10 || tel.length === 11 ) {
-        return document.getElementById("ntTel").innerHTML = ""
-    } else {
+    if (tel.length !== 10 && tel.length !== 11){
         document.getElementById("ntTel").innerHTML="Nhập lại số điện thoại đi "
+    }else {
+        return true
     }
-
-
 }
 function confirmEmail (){
-    const re =
-        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     let email = document.getElementById("email").value
     if (email.match(re)){
 
     }else if (email == ""){
-        document.getElementById("ntEmail").innerHTML="chưa nhập email kìa"
-    }else{
-        document.getElementById("ntEmail").innerHTML="chưa nhập đúng định dạng email"
+        document.getElementById("ntEmail").innerHTML="chưa nhập email "
+    }
+}
+function submitLocation(){
+    let location = document.getElementById("location").value
+    if (location == ""){
+        document.getElementById("ntLocation").innerHTML="chưa nhập địa chỉ "
+    }else {
+        return true
     }
 }
 
-
-
-
+var form = document.getElementById("myForm");
+function handleForm(event) { event.preventDefault(); }
+form.addEventListener('submit', handleForm);
 
 
 
